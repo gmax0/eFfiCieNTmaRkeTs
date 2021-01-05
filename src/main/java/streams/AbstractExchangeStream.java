5d32f48620365d83334d41f8916cb45e7f25eb7b
gaamox@tutanota.com <gaamox@tutanota.com>
2021-01-05 15:21:33 -0600

    Begin refactor of ExchangeStreams to extend from abstract class. Cleanup logging in Streams and RestAPIs. Add isEnabled flag to streams and APIs.

---
+++ src/main/java/streams/AbstractExchangeStream.java
@@ -0,0 +1,63 @@
+package streams;
+
+import buffer.OrderBookBuffer;
+import domain.constants.Exchange;
+import info.bitrich.xchangestream.core.ProductSubscription;
+import info.bitrich.xchangestream.core.StreamingExchange;
+import io.reactivex.disposables.Disposable;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.slf4j.Logger;
+
+import java.util.List;
+
+import static domain.constants.Exchange.BITFINEX;
+
+public abstract class AbstractExchangeStream {
+
+    abstract Logger getLog();
+    abstract Exchange getExchange();
+
+    boolean isEnabled;
+
+    StreamingExchange streamingExchange;
+    ProductSubscription productSubscription;
+    List<Disposable> subscriptions;
+
+    OrderBookBuffer orderBookBuffer;
+    List<CurrencyPair> currencyPairs;
+    int depth;
+
+    public void start() {
+        if (this.isEnabled) {
+            getLog().info("Initiating {}ExchangeStream WSS connection...", getExchange());
+            this.streamingExchange.connect(productSubscription).blockingAwait();
+
+            getLog().info("Creating {}ExchangeStream subscriptions...", getExchange());
+            currencyPairs.stream().forEach(currencyPair -> {
+                getLog().info("{}", currencyPair);
+                subscriptions.add(
+                        streamingExchange.getStreamingMarketDataService()
+                                .getOrderBook(currencyPair, depth)
+                                .subscribe(
+                                        (trade) -> {
+                                            //LOG.info("Trade: {}", trade);
+                                            orderBookBuffer.insert(trade, BITFINEX, currencyPair);
+                                        },
+                                        throwable -> getLog().error("Error in trade subscription", throwable)));
+            });
+        } else {
+            getLog().warn("{}ExchangeStream is disabled, not attempting WSS connection.", getExchange());
+        }
+    }
+
+    public void shutdown() {
+        if (this.isEnabled && this.streamingExchange.isAlive()) {
+            getLog().info("Disposing {}ExchangeStream subscriptions...", getExchange());
+            this.subscriptions.stream().forEach(subscription -> subscription.dispose());
+            getLog().info("Disconnecting from {}ExchangeStream WSS connection...");
+            this.streamingExchange.disconnect().blockingAwait();
+        } else {
+            getLog().info("{}ExchangeStream connection is not alive. ");
+        }
+    }
+}
